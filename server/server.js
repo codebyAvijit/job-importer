3cad1a20746bedd834a8a5ce5c0f63fa9a89b029
codeByAvijit <deviljit.ap@gmail.com>
2025-07-27 00:05:45 +0530

    ser

--- server/server.js
+++ server/server.js
@@ -11,7 +11,5 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", importRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
