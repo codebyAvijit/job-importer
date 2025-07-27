3a8ca019ff854818f5999e503d75a60b744f9d36
codeByAvijit <deviljit.ap@gmail.com>
2025-07-27 14:32:29 +0530

    updated app.jsx UI

--- client/src/components/ImportButton.jsx
+++ client/src/components/ImportButton.jsx
@@ -15,7 +15,7 @@ function ImportButton({ onImport }) {
   return (
     <button
       onClick={handleClick}
-      className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 "
     >
       Trigger Import
     </button>
